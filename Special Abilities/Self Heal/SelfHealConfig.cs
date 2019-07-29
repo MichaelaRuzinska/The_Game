5627dccd9fd79d1158f48f7104c29adf53eb8aeb
Lureithis <44930142+Lureithis@users.noreply.github.com>
2019-07-29 13:27:16 +0200

    First version

---
+++ Special Abilities/Self Heal/SelfHealConfig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace RPG.Characters
+{
+    [CreateAssetMenu(menuName = ("RPG/Special Ability/Self Heal"))] //for creating more abilities for healing
+    public class SelfHealConfig : AbilityConfig
+    {
+        [Header("Self Heal Specific")]
+        [SerializeField] float healing = 10f;
+
+        public override AbilityBehaviour getBehaviourComponent(GameObject objectToAttachTo)
+        {
+            return objectToAttachTo.AddComponent<SelfHealBehaviour>();
+        }
+
+        public float GetHealing()
+        {
+            return healing;
+        }
+    }
+}
